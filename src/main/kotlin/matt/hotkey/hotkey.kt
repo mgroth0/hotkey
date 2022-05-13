d5bbbd14db8a46f0769888c606d7a022641dd3d5
Matthew Groth <matthewgroth@matthews-mbp.myfiosgateway.com>
2022-05-13 16:59:22 -0400

    first commit

---
+++ src/main/kotlin/matt/hotkey/hotkey.kt
@@ -0,0 +1,43 @@
+package matt.hotkey
+
+import matt.klibexport.klibexport.DSL
+
+
+abstract class HotkeyDSL<H: Hotkey>: DSL {
+  abstract val SPACE: H
+
+  abstract val LEFT: H
+  abstract val RIGHT: H
+  abstract val UP: H
+  abstract val DOWN: H
+
+
+  abstract val A: H
+  abstract val B: H
+  abstract val C: H
+  abstract val D: H
+  abstract val E: H
+  abstract val F: H
+  abstract val G: H
+  abstract val H: H
+  abstract val I: H
+  abstract val J: H
+  abstract val K: H
+  abstract val L: H
+  abstract val M: H
+  abstract val N: H
+  abstract val O: H
+  abstract val P: H
+  abstract val Q: H
+  abstract val R: H
+  abstract val S: H
+  abstract val T: H
+  abstract val U: H
+  abstract val V: H
+  abstract val W: H
+  abstract val X: H
+  abstract val Y: H
+  abstract val Z: H
+}
+
+interface Hotkey
