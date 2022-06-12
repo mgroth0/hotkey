3d4279b5003912994855350892a995d661fe0417
mgroth0 <mgroth49@gmail.com>
2022-06-12 16:17:04 -0400

    autocommit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
 modtype = LIB
 
-dependencies {
-  projectOrLocalMavenJVM("api", ":k:klib")
+apis(
+  project(":k:klib")
+)
+
+//dependencies {
+//  projectOrLocalMavenJVM("api", )
 //	implementation(projects.kj.kjlib)
-}
+//}
