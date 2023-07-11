3c838a5c276029989f241fe112171fa2b1cce8c4
mgroth0 <mgroth49@gmail.com>
2023-07-11 19:25:22 -0400

    added 2 files,changed build.json,removed src/main/kotlin/matt/hotkey/hotkey.kt

---
+++ src/jsMain/kotlin/matt/hotkey/hotkey.kt
@@ -0,0 +1,7 @@
+package matt.hotkey
+
+import matt.lang.anno.SeeURL
+import org.w3c.dom.events.KeyboardEvent
+
+@SeeURL("https://stackoverflow.com/questions/24386354/execute-js-code-after-pressing-the-spacebar")
+val KeyboardEvent.isSpace get() = key == " " || code == "Space" || keyCode == 32
